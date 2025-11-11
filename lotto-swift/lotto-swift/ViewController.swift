b309985b17a6491b725959507f838f5428f16ae8
JohnPrk <uo3641493@gmail.com>
2025-11-12 04:18:41 +0900

    feat: 로또 및 로또 생성 관련 도메인 모델 추가 및 스크롤 뷰 세팅 및 뷰에 구매한 개수만큼 로또 출력
    - Lotto 및 LottoGenerator 도메인 모델 추가
    - 구입 금액을 기반으로 구매한 개수만큼 로또를 생성하여 뷰에 출력
    - ScrollView 뷰를 추가하여 스크롤 가능하게 처리

--- lotto-swift/lotto-swift/ViewController.swift
+++ lotto-swift/lotto-swift/ViewController.swift
@@ -4,15 +4,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var purchaseAmountMessageLabel: UILabel!
     @IBOutlet weak var amountTextField: UITextField!
     @IBOutlet weak var resultLabel: UILabel!
     @IBOutlet weak var confirmButton: UIButton!
+    @IBOutlet weak var lottoLabel: UILabel!
+
+    private let lottoGenerator: LottoGenerator = RandomLottoGenerator()
+    private var lottos: [Lotto] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         resultLabel.text = ""
+        lottoLabel.text = ""
+        lottoLabel.numberOfLines = 0
+        lottoLabel.lineBreakMode = .byWordWrapping
     }
 
     @IBAction func didTapConfirmButton(_ sender: UIButton) {
@@ -24,12 +33,22 @@ class ViewController: UIViewController {
             let amount = try InputValidator.parsePurchaseAmount(amountTextField.text)
             let money = try Money(amount: amount)
             let lottoCount = money.lottoCount
-            resultLabel.text = "\(lottoCount)개를 구매했습니다."
             confirmButton.isEnabled = false
             confirmButton.alpha = 0.5
             amountTextField.isEnabled = false
             view.endEditing(true)
-        } catch let error as InputError {
+            lottos = try lottoGenerator.generate(count: lottoCount)
+            let ticketsText = lottos
+                .map { lotto in
+                    let nums = lotto.getNumbers()
+                        .map { String($0) }
+                        .joined(separator: ", ")
+                    return "[\(nums)]"
+                }
+                .joined(separator: "\n")
+            resultLabel.text = "\(lottoCount)개를 구매했습니다."
+            lottoLabel.text = ticketsText
+       } catch let error as InputError {
             showAlert(message: error.localizedDescription)
             resetInput()
         } catch let error as MoneyError {
@@ -53,4 +72,3 @@ class ViewController: UIViewController {
         present(alert, animated: true, completion: nil)
     }
 }
-
